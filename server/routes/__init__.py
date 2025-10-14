48b346d6045fa1b2d6cf4f5dbb3ad4296c5a3eb1
Vikas Verma <vikas.verma@gameskraft.com>
2025-10-14 20:18:01 +0530

    attach routing

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -5,10 +5,12 @@ from fastapi import APIRouter
 from .chat import router as chat_router
 from .gmail import router as gmail_router
 from .meta import router as meta_router
+from .composio import router as composio_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(meta_router)
 api_router.include_router(chat_router)
 api_router.include_router(gmail_router)
+api_router.include_router(composio_router)
 
 __all__ = ["api_router"]
